40fad8331d890ee4ef641939d98d2855a171c019
anotherik <ricardojotagoncalves@hotmail.com>
2016-11-30 04:54:15 +0000

    changing test to fail

--- Jenkins/Prolog/unittests2.pl
+++ Jenkins/Prolog/unittests2.pl
@@ -1,3 +1,6 @@
+#!/usr/bin/swipl -f -q
+:- initialization main.
+
 :- begin_tests(lists).
 :- use_module(library(lists)).
 
@@ -7,8 +10,9 @@ test(reverse) :-
 test(a) :-
         A is 2^3,
         assertion(integer(A)),
-        assertion(A == 8).
+        assertion(A == 9).
 
 :- end_tests(lists).
 
-:-    run_tests.
+main :- run_tests,
+	    halt(0).
